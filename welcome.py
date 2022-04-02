ca9e3fbad69b6e334c3eaa9f10933236cd460762
priyanka <priyanka.chavan1408@gmail.com>
2022-04-02 17:12:11 +0530

    [Priyanka]Added:Checked employee is present or not.

--- welcome.py
+++ welcome.py
@@ -1,1 +1,19 @@
-print("Welcome message")
+'''
+@Author : Priyanka
+@Date : 2022-04-02  16:29:00
+@Last Modified by : Priyanka
+@Last Modified Time : 2022-04-02  16:35:00
+@Title : Checking employee is present or not.
+'''
+
+import random
+
+print("Welcome to the employee wage builder program.")
+
+IS_Full_Time = 1
+empCheck = random.randint(0,1)
+
+if empCheck == IS_Full_Time:
+    print("Employee is present")
+else:
+    print("Employee is absent")
